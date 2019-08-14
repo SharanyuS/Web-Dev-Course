5ae1853cdec3a4962949b6874bb5048f71e5b58d
Sharanyu S <42795187+SharanyuS@users.noreply.github.com>
2019-08-14 21:40:25 +0530

    Add files via upload

---
+++ g.php
@@ -0,0 +1,85 @@
+<?php
+
+// Demand a GET parameter
+if ( ! isset($_GET['name']) || strlen($_GET['name']) < 1  ) {
+    die('Name parameter missing');
+}
+
+// If the user requested logout go back to index.php
+if ( isset($_POST['logout']) ) {
+    header('Location: i.php');
+    return;
+}
+
+// Set up the values for the game...
+// 0 is Rock, 1 is Paper, and 2 is Scissors
+$names = array('Rock', 'Paper', 'Scissors');
+$human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
+
+$computer = 0; // Hard code the computer to rock
+// TODO: Make the computer be random
+$computer=rand(0,2);
+function check($computer, $human) {
+ 
+    if ( $human == $computer ) {
+        return "Tie";
+    } else if ( ($human==0 AND $computer==1)  OR ( $human==2 AND $computer==0) OR ($human==1 AND $computer==2)){
+        return "You Lose";
+    }
+     else if ( ($human==2 AND $computer==1) OR ($human==1 AND $computer==0) OR ($human==0 AND $computer==2)){
+        return "You Win";
+    } 
+    return false;
+}
+
+// Check to see how the play happenned
+$result = check($computer, $human);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Deepanshu Sehgal e5eded35</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Rock Paper Scissors</h1>
+<?php
+if ( isset($_REQUEST['name']) ) {
+    echo "<p>Welcome: ";
+    echo htmlentities($_REQUEST['name']);
+    echo "</p>\n";
+}
+?>
+<form method="post">
+<select name="human">
+<option value="-1">Select</option>
+<option value="0">Rock</option>
+<option value="1">Paper</option>
+<option value="2">Scissors</option>
+<option value="3">Test</option>
+</select>
+<input type="submit" value="Play">
+<input type="submit" name="logout" value="Logout">
+</form>
+
+<pre>
+<?php
+if ( $human == -1 ) {
+    print "Please select a strategy and press Play.\n";
+} else if ( $human == 3 ) {
+    for($c=0;$c<3;$c++) {
+        for($h=0;$h<3;$h++) {
+            $r = check($c, $h);
+            print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
+        }
+    }
+} else {
+    print "Your Play=$names[$human] Computer Play=$names[$computer] Result=$result\n";
+}
+?>
+</pre>
+</div>
+</body>
+</html>
---
+++ i.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Deepanshu Sehgal e5eded35</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="l.php">Please Log In</a>
+</p>
+<p>
+<a href="g.php">game.php</a> 
+</div>
+</body>
+</p>
+</html>
