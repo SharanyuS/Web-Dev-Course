32dfd7c7f998beef9e81907d22d18f932a39f6c9
Sharanyu S <42795187+SharanyuS@users.noreply.github.com>
2019-08-15 12:04:01 +0530

    Update l.php

--- l.php
+++ l.php
@@ -32,7 +32,7 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title> Deepanshu Sehgal e5eded35</title>
+<title>nYuker</title>
 </head>
 <body>
 <div class="container">
